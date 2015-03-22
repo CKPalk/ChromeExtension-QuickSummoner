edf685adcc44d05d69f78cc8f5b791d9d1d3a53c
CKPalk <ckpalk@comcast.net>
2015-03-22 16:02:01 -0700

    Started Top Three Champion Section
    Added a section to display the summoners top three most played ranked
    champions. Will eventually add game record for each and misc. stats.
    TODO next: images for the champion, will need local files

--- QuickSummonerJS.js
+++ QuickSummonerJS.js
@@ -15,6 +15,7 @@ function getSummoner(name, serverCode) {
 		var summoner_NAME = summoner_result.name;
 		
 		getRankedTier(summoner_ID, serverCode);
+		getTopChamps(summoner_ID, serverCode);
 		
 		var summoner_LVL = summoner_result.summonerLevel;
 		document.getElementById('content_summonerNAME').innerHTML = summoner_NAME;
@@ -36,6 +37,7 @@ function getTopGameCounts(summonerID, serverCode) {
 		
 		
 	}
+	xmlhttp.send();
 }
 
 function setPromoImg(game_num, game_outcome) {
@@ -69,14 +71,17 @@ function getRankedTier(summonerID, serverCode) {
 					document.getElementById('promo_game_' + i).hidden = false;
 					setPromoImg(i, games_outcome.charAt(i - 1));
 				}
-				if (summoner_result.entries[0].miniSeries.target > 2) {
+				if (summoner_result.entries[0].miniSeries.target == 3) {
 					document.getElementById('promo_game_4').hidden = false;
 					document.getElementById('promo_game_5').hidden = false;
 					setPromoImg(4, games_outcome.charAt(3));
+					setPromoImg(5, games_outcome.charAt(4));
 				}
 				else {
 					document.getElementById('promo_game_4').hidden = true;
 					document.getElementById('promo_game_5').hidden = true;
+					setPromoImg(4, "N");
+					setPromoImg(5, "N");
 				}
 			}
 			else {
@@ -90,9 +95,12 @@ function getRankedTier(summonerID, serverCode) {
 				document.getElementById('promo_game_5').hidden = true;
 				document.getElementById('series_text').innerHTML = "";
 			}
+			
 			var summoner_LP_text;
 			if (summoner_LP == 100)
 				summoner_LP_text = "1<br>0<br>0";
+			else if (summoner_LP < 10)
+				summoner_LP_text = summoner_LP + "";
 			else
 				summoner_LP_text = (Math.floor(summoner_LP / 10)) + "<br>" + (summoner_LP % 10);
 				
@@ -117,7 +125,7 @@ function getRankedTier(summonerID, serverCode) {
 			document.getElementById('content_window1').hidden = false;
 			document.getElementById('content_window2').hidden = false;
 		}
-		else {
+		if (xmlhttp.status == 404) {
 			for (var i = 1; i <= 5; i++) {
 				setPromoImg(i, "N");
 			}
@@ -129,14 +137,89 @@ function getRankedTier(summonerID, serverCode) {
 			document.getElementById('series_text').innerHTML = "";
 			document.getElementById('content_summonerRANK_img').src = "ranked_img/unknown.png";
 			document.getElementById('content_summonerRANK_text').innerHTML = "Unranked";
-			document.getElementById('content_summonerLP_result').style.height = "8px";
+			document.getElementById('content_summonerLP_low_result').innerHTML = "";
 			document.getElementById('content_summonerLP_result').innerHTML = "";
+			document.getElementById('content_summonerLP_result').style.height = "8px";
 			document.getElementById('content_window1').hidden = false;
 			document.getElementById('content_window2').hidden = false;
 		}
 	}
 	
 	xmlhttp.send();
+}
+
+function getTopChamps(summoner_ID, serverCode) {
+		var xmlhttp = new XMLHttpRequest();
+		// https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/33011944/ranked?&api_key=7d6ae555-333f-48af-b119-1b2fa5d415f9
+		var url = 'https://' + serverCode + '.api.pvp.net/api/lol/' + serverCode + '/v1.3/stats/by-summoner/' + summoner_ID + '/ranked?&api_key=7d6ae555-333f-48af-b119-1b2fa5d415f9';
+		xmlhttp.open('GET', url);
+		xmlhttp.onload = function() {
+			if (xmlhttp.status == 200) {
+				var response = JSON.parse(xmlhttp.response);
+				var champions = response.champions;
+				var champ1_index, champ2_index, champ3_index;
+				var champCount = champions.length;
+				var topValue = 0;
+
+				for (var i = 0; i < champCount; i++) {
+					if (champions[i].id != 0) {
+						if (champions[i].stats.totalSessionsPlayed > topValue) {
+							topValue = champions[i].stats.totalSessionsPlayed;
+							champ1_index = i;
+						}
+					}
+				}
+				topValue = 0;
+				for (var i = 0; i < champCount; i++) {
+					if (i != champ1_index && champions[i].id != 0) {
+						if (champions[i].stats.totalSessionsPlayed > topValue) {
+							topValue = champions[i].stats.totalSessionsPlayed;
+							champ2_index = i;
+						}
+					}
+				}
+				topValue = 0;
+				for (var i = 0; i < champCount; i++) {
+					if (i != champ1_index && i != champ2_index && champions[i].id != 0) {
+						if (champions[i].stats.totalSessionsPlayed > topValue) {
+							topValue = champions[i].stats.totalSessionsPlayed;
+							champ3_index = i;
+						}
+					}
+				}
+				var champ1 = champions[champ1_index];
+				var champ2 = champions[champ2_index];
+				var champ3 = champions[champ3_index];
+				setChampStat(1 , champ1.id);
+				setChampStat(2 , champ2.id);
+				setChampStat(3 , champ3.id);
+				document.getElementById('ChampStats').hidden = false;
+			}
+			else {
+				document.getElementById('ChampStats').hidden = true;
+				setChampStat(1 , 0);
+				setChampStat(2 , 0);
+				setChampStat(3 , 0);
+			}
+		}
+		xmlhttp.send();
+}
+
+function setChampStat(slot, champ_ID) {
+	if (champ_ID != 0) {
+		var xmlhttp = new XMLHttpRequest();
+		// https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/40?&api_key=7d6ae555-333f-48af-b119-1b2fa5d415f9
+		var url = 'https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/' + champ_ID + '?&api_key=7d6ae555-333f-48af-b119-1b2fa5d415f9';
+		xmlhttp.open('GET', url);
+		xmlhttp.onload = function() {
+			var response = JSON.parse(xmlhttp.response);
+			document.getElementById('champ' + slot + '_name').innerHTML = response.name;
+		}
+		xmlhttp.send();
+	}
+	else {
+		document.getElementById('champ' + slot + '_name').innerHTML = "";
+	}
 }
 
 /*
@@ -165,7 +248,8 @@ function getMatchHistory(summonerID) {
 var server = "NA";
 document.addEventListener('DOMContentLoaded', function() {
 	var summoner_input_value = document.getElementById('summoner_input_value');
-
+	document.getElementById('summoner_input_value').focus();
+	document.getElementById('summoner_input_value').addEventListener('click', function() { this.value = ""; });
 	document.getElementById('summoner_input_value').addEventListener('keypress', function(e) {
 		if (e.keyCode === 13) {
 			server = document.getElementById('server').value;
